bda4f6f92ed83c133225fea0f8628b92641536e7
Enijar <TheEnijar@gmail.com>
2021-02-10 08:47:08 +0000

    wip: rotate player to point at raycaster

--- src/game/player/player.tsx
+++ src/game/player/player.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import * as THREE from "three";
 import { OrthographicCamera, useTexture } from "@react-three/drei";
 import { useGame } from "../state";
 import useControls from "../hooks/use-controls";
 import shape from "../shape";
+import { useFrame } from "react-three-fiber";
 
 export default function Player() {
+  const playerMesh = React.useRef<THREE.Mesh>();
   const [size, setSize] = React.useState<number>(0);
   const [zoom] = React.useState<number>(1);
   const { player } = useGame();
@@ -15,6 +18,7 @@ export default function Player() {
     function onResize() {
       setSize(Math.max(window.innerWidth, window.innerHeight));
     }
+
     onResize();
     window.addEventListener("resize", onResize);
     return () => {
@@ -22,9 +26,17 @@ export default function Player() {
     };
   }, []);
 
+  useFrame((state) => {
+    if (!playerMesh.current) return;
+    const { x, y } = state.raycaster.ray.origin;
+    const angle = Math.atan2(x - (player.position.x/2), -(y - player.position.y));
+    playerMesh.current.rotation.set(0, 0, angle);
+    console.log(angle);
+  });
+
   return (
     <group position={[player.position.x, player.position.y, 0]}>
-      <mesh>
+      <mesh ref={playerMesh}>
         <planeBufferGeometry attach="geometry" args={[0.1, 0.1, 1]} />
         <meshBasicMaterial attach="material" map={texture} transparent />
       </mesh>

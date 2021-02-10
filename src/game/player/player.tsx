52a084040b1e7bfb6ee191e22542e79729cd1f8c
Enijar <TheEnijar@gmail.com>
2021-02-10 19:04:35 +0000

    fix(performance): update player rotation only when pointer moves

--- src/game/player/player.tsx
+++ src/game/player/player.tsx
@@ -4,11 +4,12 @@ import { OrthographicCamera, useTexture } from "@react-three/drei";
 import { useGame } from "../state";
 import useControls from "../hooks/use-controls";
 import shape from "../shape";
-import { useFrame } from "react-three-fiber";
+import { useThree } from "react-three-fiber";
 import { deg2rad } from "../utils";
 
+const box = new THREE.Box3();
+
 export default function Player() {
-  const box = React.useMemo<THREE.Box3>(() => new THREE.Box3(), []);
   const playerGroup = React.useRef<THREE.Group>();
   const playerMesh = React.useRef<THREE.Mesh>();
   const [size, setSize] = React.useState<number>(0);
@@ -16,6 +17,7 @@ export default function Player() {
   const { player } = useGame();
   const texture = useTexture(shape(player.shape, player.color));
   useControls();
+  const { raycaster } = useThree();
 
   React.useEffect(() => {
     function onResize() {
@@ -29,14 +31,21 @@ export default function Player() {
     };
   }, []);
 
-  useFrame((state) => {
-    if (!playerGroup.current || !playerMesh.current) return;
-    box.setFromObject(playerGroup.current);
-    const cX = (box.max.x + box.min.x) / 2;
-    const cY = (box.max.y + box.min.y) / 2;
-    const { x, y } = state.raycaster.ray.origin;
-    playerMesh.current.rotation.z = Math.atan2(y - cY, x - cX) - deg2rad(90);
-  });
+  React.useEffect(() => {
+    function onMove() {
+      if (!playerMesh.current) return;
+      if (!playerGroup.current || !playerMesh.current) return;
+      box.setFromObject(playerGroup.current);
+      const cX = (box.max.x + box.min.x) / 2;
+      const cY = (box.max.y + box.min.y) / 2;
+      const { x, y } = raycaster.ray.origin;
+      playerMesh.current.rotation.z = Math.atan2(y - cY, x - cX) - deg2rad(90);
+    }
+    window.addEventListener("pointermove", onMove);
+    return () => {
+      window.removeEventListener("pointermove", onMove);
+    };
+  }, [raycaster]);
 
   return (
     <group

3d63aa6289064b37f566cd677ce16db24165a063
Enijar <TheEnijar@gmail.com>
2021-02-17 18:23:49 +0000

    fix(server): remove old engine

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,13 @@
-import { NewPlayer } from "../../client/src/shared/types";
 import config from "./config/config";
 import database from "./services/database";
 import { http, socket as io } from "./services/app";
-import engine, { GameEngineContext } from "../../client/src/shared/game/engine";
 
 (async () => {
   try {
     await database.sync({ alter: true });
 
-    engine.context = GameEngineContext.server;
-    engine.io = io;
-
     io.on("connection", (socket) => {
-      let playerId: number = -1;
-
-      engine.socket = socket;
-
-      socket.on("newPlayer.connect", (newPlayer: NewPlayer) => {
-        playerId = engine.addNewPlayer(newPlayer);
-      });
-
-      socket.on("disconnect", () => {
-        engine.removePlayer(playerId);
-      });
+      console.log(socket.id);
     });
 
     http.listen(config.port, () => {

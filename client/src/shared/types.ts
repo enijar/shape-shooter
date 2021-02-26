fc407986d92384f0981b32ac88f793e1e4158a4b
Enijar <TheEnijar@gmail.com>
2021-02-26 07:04:48 +0000

    chore: remove unused types

--- client/src/shared/types.ts
+++ client/src/shared/types.ts
@@ -11,78 +11,11 @@ export enum Weapon {
   handgun = "handgun",
 }
 
-export type NewPlayer = {
-  name: string;
-  shape: Shape;
-  weapon: Weapon;
-  color: string;
-};
-
 export enum ControlsKeys {
   moveUp = "w",
   moveDown = "s",
   moveLeft = "a",
   moveRight = "d",
-}
-
-export type Bullet = {
-  id: number;
-  playerId: number;
-  sX: number;
-  sY: number;
-  x: number;
-  y: number;
-  r: number;
-};
-
-export type Player = {
-  id: number;
-  name: string;
-  x: number;
-  y: number;
-  r: number;
-  shape: Shape;
-  color: string;
-};
-
-export type ShotPayload = {
-  playerId: number;
-  bullets: Bullet[];
-};
-
-export type State = {
-  players: Player[];
-  bullets: Bullet[];
-};
-
-export enum GameActionType {
-  playerRotate,
-  playerMove,
-  playerFire,
-}
-
-export type GameAction = {
-  type: GameActionType;
-  payload: any;
-};
-
-export enum GameEventType {
-  playerConnected,
-  playerDisconnected,
-  playerHp,
-  playerFire,
-  playerRotate,
-  playerMove,
-}
-
-export type GameEvent = {
-  type: GameEventType;
-  payload: any;
-};
-
-export enum GameContext {
-  client,
-  server,
 }
 
 export type BulletData = {

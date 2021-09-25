954e3686c38a31171e972891f3009732df9071e9
Enijar <TheEnijar@gmail.com>
2021-09-25 14:02:27 +0100

    fix: client server service config

--- client/src/services/server.ts
+++ client/src/services/server.ts
@@ -1,7 +1,7 @@
 import geckos from "@geckos.io/client";
 import config from "../config";
 
-const channel = geckos({ url: config.apiUrl, port: 3000 });
+const channel = geckos({ url: config.apiUrl, port: config.serverPort });
 
 type Data = string | number | Object | null;
 

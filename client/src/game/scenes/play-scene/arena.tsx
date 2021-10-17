4b069a6b42e546daea82831d1a5b3e296c5a0c25
Enijar <TheEnijar@gmail.com>
2021-10-17 16:37:28 +0100

    feat: animate arena

--- client/src/game/scenes/play-scene/arena.tsx
+++ client/src/game/scenes/play-scene/arena.tsx
@@ -2,25 +2,32 @@ import React from "react";
 import { settings } from "@app/shared";
 import { Instance, Instances } from "@react-three/drei";
 import { Position } from "@react-three/drei/helpers/Position";
+import { useFrame } from "@react-three/fiber";
+import { MathUtils } from "three";
 
 const SIZE = settings.arena.size;
 const CELL = settings.arena.cell;
 
-const maxTiles = (SIZE / CELL.size) ** 2;
-const tiles: null[] = [];
-for (let i = 0; i < maxTiles; i++) {
-  tiles.push(null);
-}
+const maxCells = (SIZE / CELL.size) ** 2;
+const cells = Array.from({ length: maxCells });
 
 function Arena() {
   const cellRefs = React.useRef<Position[]>([]);
 
+  useFrame(({ clock }) => {
+    const d = (1 + Math.sin(clock.getElapsedTime())) / 2;
+    const s = MathUtils.mapLinear(d, 0, 1, 0.5, 0.8);
+    for (let i = 0; i < maxCells; i++) {
+      cellRefs.current[i].scale.setScalar(s);
+    }
+  });
+
   return (
     <group position={[SIZE * -0.5, SIZE * -0.5, -1]}>
-      <Instances limit={maxTiles} range={maxTiles}>
+      <Instances limit={maxCells} range={maxCells}>
         <planeBufferGeometry args={[CELL.size, CELL.size]} />
         <meshBasicMaterial color="#333333" />
-        {tiles.map((_, index) => {
+        {cells.map((_, index) => {
           const col = Math.floor(index % (SIZE / CELL.size));
           const row = Math.floor(index / (SIZE / CELL.size));
           const x = (CELL.size + CELL.gap) * col;

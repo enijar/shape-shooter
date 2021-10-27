0849ef4d030767ef8a03ecdafd27e729f7ffea33
Enijar <TheEnijar@gmail.com>
2021-10-27 20:53:31 +0100

    wip: custom socket encode/decode

--- shared/src/game/parser.ts
+++ shared/src/game/parser.ts
@@ -1,4 +1,55 @@
+// import * as parser from "socket.io-parser";
+import Emitter from "component-emitter";
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
 // @ts-ignore
 import * as parser from "socket.io-msgpack-parser";
 
-export default parser;
+class Encoder {
+  encode(packet: any) {
+    return [encoder.encode(JSON.stringify(packet))];
+  }
+}
+
+class Decoder extends Emitter {
+  /**
+   * Receive a chunk (string or buffer) and optionally emit a "decoded" event with the reconstructed packet
+   */
+  add(chunk: ArrayBufferLike) {
+    const packet = JSON.parse(decoder.decode(chunk));
+    if (this.isPacketValid(packet)) {
+      this.emit("decoded", packet);
+    } else {
+      throw new Error("invalid format");
+    }
+  }
+  isPacketValid({ type, data, nsp, id }: any) {
+    const isNamespaceValid = typeof nsp === "string";
+    const isAckIdValid = id === undefined || Number.isInteger(id);
+    if (!isNamespaceValid || !isAckIdValid) {
+      return false;
+    }
+    switch (type) {
+      case 0: // CONNECT
+        return data === undefined || typeof data === "object";
+      case 1: // DISCONNECT
+        return data === undefined;
+      case 2: // EVENT
+        return Array.isArray(data) && data.length > 0;
+      case 3: // ACK
+        return Array.isArray(data);
+      case 4: // CONNECT_ERROR
+        return typeof data === "object";
+      default:
+        return false;
+    }
+  }
+  /**
+   * Clean up internal buffers
+   */
+  destroy() {}
+}
+
+export default { Encoder, Decoder };
